8de789922b065023decf212c180e296a66fbc8a7
brendapsd <brenda.sd.15@gmail.com>
2019-02-28 20:00:28 -0500

    funcion de evaluar si es un directorio o no y su tesst

--- test/path.spec.js
+++ test/path.spec.js
@@ -1,13 +1,22 @@
-import { convertRelativeToAbsolute, pathIsAbsolute, pathIsFile } from '../lib/controller/path.js'
+import { convertRelativeToAbsolute, pathIsAbsolute, pathIsDirectory } from '../lib/controller/path.js'
 
 describe('convertRelativeToAbsolute', () => {
     it ('Deberia ser una función', () => {
         expect(typeof convertRelativeToAbsolute).toBe('function')
     })
-    it ('Deberia ser retornar false ya que la ruta es relativa', () => {
+    it ('Deberia retornar false ya que la ruta es relativa', () => {
         expect(pathIsAbsolute('.\\test')).toBe(false)
+    })
+    it ('Deberia retornar true ya que la ruta es relativa', () => {
+        expect(pathIsAbsolute('C:\\Users\\brenda\\Documents\\project markdown\\LIM008-fe-md-links\\test')).toBe(true)
     })
     it ('Deberia convertir una ruta relativa a absoluta', () => {
         expect(convertRelativeToAbsolute('.\\test')).toBe('C:\\Users\\brenda\\Documents\\project markdown\\LIM008-fe-md-links\\test')
+    })
+    it ('Deberia retornar true ya que la ruta es directorio', () => {
+        expect(pathIsDirectory('.\\test')).toBe(true)
+    })
+    it ('Deberia retornar true ya que la ruta es directorio', () => {
+        expect(pathIsDirectory('.\\test\\testPrueba\\file.js')).toBe(false)
     })
 })
